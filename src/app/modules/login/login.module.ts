1e2ca1976fc546256c1de085c7527f5a8ac5d3a3
Ravi Akhouri <ravi.akhouri@gmail.com>
2022-09-01 01:17:22 +0530

    UI design in progress

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoginRoutingModule } from './login-routing.module';
-
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
+    SharedModule,
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
   ]
 })
 export class LoginModule { }
