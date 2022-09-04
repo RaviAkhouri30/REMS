873d66f6d2877a5f8af676268ee92cf71f64bce9
Ravi Akhouri <ravi.akhouri@gmail.com>
2022-09-05 01:43:16 +0530

    Login Page finalised

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,12 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   private userName: string;
+  @ViewChild('hamMenu') private hamMenu!: ElementRef;
 
   constructor() {
     this.userName = 'Hi, Guest';
   }
 
   ngOnInit(): void {
+  }
+
+  public showMenu = (): void => {
+    const _div = this.hamMenu.nativeElement as HTMLDivElement;
+    if (_div.style.display === 'none') {
+      _div.style.display = 'block';
+      return;
+    }
+    _div.style.display = 'none';
   }
 
 }
