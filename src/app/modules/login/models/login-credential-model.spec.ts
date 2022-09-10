0d102b7a67aa5a9d7cb819c0e7eaf23178ba2423
Ravi Akhouri <ravi.akhouri@gmail.com>
2022-09-10 13:59:48 +0530

    Validation and minor design changes completed

---
+++ src/app/modules/login/models/login-credential-model.spec.ts
@@ -0,0 +1,7 @@
+import { LoginCredentialModel } from './login-credential-model';
+
+describe('LoginCredentialModel', () => {
+  it('should create an instance', () => {
+    expect(new LoginCredentialModel()).toBeTruthy();
+  });
+});
